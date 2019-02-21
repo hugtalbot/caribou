c4e0babe8ee90ab11ba4e5eeaa979ac2c0027f08
Jean-Nicolas Brunet <jnbrunet2000@gmail.com>
2019-02-21 18:41:41 +0100

    [SofaCaribou] System exporter part 2

--- src/SofaCaribou/GraphComponents/IO/SystemExporter.cpp
+++ src/SofaCaribou/GraphComponents/IO/SystemExporter.cpp
@@ -4,6 +4,14 @@
 
 namespace SofaCaribou::GraphComponents::IO {
 
+SystemExporter::SystemExporter()
+: d_ode_solvers(initLink("ode_solvers", "Links to OdeSolver objects from which the ODE system will be retrieved. If none"
+                                        " is provided, this component will search in its current context (ignoring child"
+                                        " nodes) for ODE solvers."))
+{
+
+};
+
 void SystemExporter::init ()
 {
     
--- src/SofaCaribou/GraphComponents/IO/SystemExporter.h
+++ src/SofaCaribou/GraphComponents/IO/SystemExporter.h
@@ -2,15 +2,31 @@
 #define SOFACARIBOU_GRAPHCOMPONENTS_IO_SYSTEMEXPORTER_H
 
 #include <sofa/core/objectmodel/BaseObject.h>
+#include <sofa/core/behavior/OdeSolver.h>
 
 namespace SofaCaribou::GraphComponents::IO {
+
+using namespace sofa::core::objectmodel;
+using namespace sofa::core::behavior;
 
 class SystemExporter : public sofa::core::objectmodel::BaseObject
 {
 public:
     SOFA_CLASS(SystemExporter, BaseObject);
 
+    template <typename ObjectType>
+    using MultiLink = MultiLink<SystemExporter, ObjectType, BaseLink::FLAG_NONE>;
+
+    SystemExporter();
+
     void init() override;
+
+private:
+    /**
+     * Links to OdeSolver objects from which the ODE system will be retrieved.
+     * If none is provided, this component will search in its current context (ignoring child nodes) for ODE solvers.
+     */
+    MultiLink<OdeSolver> d_ode_solvers;
 };
 
 } // namespace SofaCaribou::GraphComponents::IO
